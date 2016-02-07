64ac53c8788dc3c0d501076309b0564e96306e43
Ian.Lintner <lintner.ian@gmail.com>
2016-02-07 01:03:37 -0600

    updated script

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 CURRENT_DIRECTORY := $(shell pwd)
 
 build:
-	@docker build --tag=espressodev/apache-drupal:latest $(CURRENT_DIRECTORY)
+	@docker build --tag=espressodev/apache-fpm-drupal:latest $(CURRENT_DIRECTORY)
 
 build-no-cache:
-	@docker build --no-cache --tag=espressodev/apache-drupal:latest $(CURRENT_DIRECTORY)
+	@docker build --no-cache --tag=espressodev/apache-fpm-drupal:latest $(CURRENT_DIRECTORY)
 
 .PHONY: build
 
--- run.sh
+++ run.sh
@@ -3,21 +3,21 @@ set -e
 
 if [ ! -z ${GIT_REPO} ]; then
   if [ ! -f /var/www/drupal/.git/config ]; then
-    if [ -z ${GIT_BRANCH} ] then
+    if [ -z ${GIT_BRANCH} ]; then
       git clone $GIT_REPO /var/www/drupal
     else
       git clone -b $GIT_BRANCH $GIT_REPO /var/www/drupal
     fi
   else
-    if [ -z ${GIT_BRANCH} ] then
+    if [ -z ${GIT_BRANCH} ]; then
       git pull
     else
       git checkout $GIT_BRANCH
     fi
   fi
-else if [ ! -z ${SITE_INSTALL} ]; then
+elif [ ! -z ${SITE_INSTALL} ]; then
    if [ ! -f /var/www/drupal/docroot/index.php ]; then
-     drush dl drupal7 --destination=/var/www/drupal/docroot
+     drush dl drupal-7.x --destination=/var/www/drupal --drupal-project-rename=docroot
      cd /var/www/drupal/docroot
      drush site-install standard --account-name=$DRUPAL_USER --account-pass=$DRUPAL_PASSWORD --db-url=mysql://$MYSQL_USER:$MYSQL_PASSWORD@$MYSQL_HOST/$MYSQL_DATABASE
    fi
